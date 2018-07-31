51624cc0bbf6a2eb9ce92740468f107a3185a090
Tore Alvestad <tore.alvestad@gmail.com>
2018-07-31 14:36:03 +0200

    Added my birthday page

--- application.py
+++ application.py
@@ -1,3 +1,5 @@
+import datetime
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -17,3 +19,9 @@ def friends():
 @app.route("/music")
 def music():
     return render_template("music.html")
+
+@app.route("/birthday")
+def birthday():
+    now = datetime.datetime.now()
+    my_birthday = now.month == 12 and now.day == 24
+    return render_template("birthday.html")
